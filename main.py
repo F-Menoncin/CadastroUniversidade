08c7944468777ea964fbd95e02052f0aedc3cc23
Felipe <felipemenoncin1@gmail.com>
2025-04-01 17:39:42 -0300

    semana 2 if/elif

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-opcao = 0
-while opcao != 6:
 #apresentando o menu inicial
-  print("Bem vindo ao menu!")
-  print("1. Estudantes")
-  print("2. Professores")
-  print("3. Disciplinas")
-  print("4. Turmas")
-  print("5. Matrículas")
-  print("6. Sair")
+print("Bem vindo ao menu!")
+print("1. Estudantes")
+print("2. Professores")
+print("3. Disciplinas")
+print("4. Turmas")
+print("5. Matrículas")
+print("6. Sair")
 
-  #recebendo a resposta do usuário (int para receber a variavel no formato numero ao invés de texto)
-  opcao = int(input("Digite o número da opção desejada: "))
-  opcao_secundaria = 0
-  while opcao_secundaria != 5:
-    #apresentando o segundo menu
-    if opcao == 6 :
+#recebendo a resposta do usuário (int para receber a variavel no formato numero ao invés de texto)
+opcao = int(input("Digite o número da opção desejada: "))
+
+  #apresentando o segundo menu
+if opcao == 6 :
       print("Obrigado e volte sempre!")
-      break
-    elif 1 <= opcao < 6:
-      print(f"MENU DE OPERAÇÕES - Opção {opcao}.")
-      print("Seleciona uma ação para prosseguir: ")
-      print("1. Incluir")
-      print("2. Listar")
-      print("3. Excluir")
-      print("4. Alterar")
-      print("5. Returnar ao menu inicial")
-      opcao_secundaria = int(input("Selecione uma das opções acima: "))
-      if 1 <= opcao_secundaria < 5 :
+elif 1 <= opcao < 6:
+    print(f"MENU DE OPERAÇÕES - Opção {opcao}.")
+    print("Seleciona uma ação para prosseguir: ")
+    print("1. Incluir")
+    print("2. Listar")
+    print("3. Excluir")
+    print("4. Alterar")
+    print("5. Returnar ao menu inicial")
+    opcao_secundaria = int(input("Selecione uma das opções acima: "))
+    if 1 <= opcao_secundaria < 5 :
         print(f"Você selecionou a opção {opcao_secundaria}.")
-      else :
+    else :
         print("Opção inválida!")
-    else:
-      print("Opção inválida!")
-
-
-
+else:
+  print("Opção inválida!")
