2749f3a53cc3cf24114f3268cda531cae9f95a70
Felipe <felipemenoncin1@gmail.com>
2025-04-01 20:18:16 -0300

    EM DESENVOLVIMENTO

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ while True:
   print("5. Matrículas")
   print("6. Sair")
 
-    #recebendo a resposta do usuário (int para receber a variavel no formato numero ao invés de texto)
-  opcao = int(input("Digite o número da opção desejada: "))
-
+  #recebendo a resposta do usuário (int para receber a variavel no formato numero ao invés de texto)
+  try:
+    opcao = int(input("Digite o número da opção desejada: "))
+  except ValueError:
+    opcao = -1
       #apresentando o segundo menu
   if opcao == 6:
       break
-  else : 
+  elif  opcao == 1: 
       while True:
         print(f"MENU DE OPERAÇÕES - Opção {opcao}.")
         print("Seleciona uma ação para prosseguir: ")
@@ -23,9 +25,20 @@ while True:
         print("3. Excluir")
         print("4. Alterar")
         print("5. Returnar ao menu principal")
-        opcao_secundaria = int(input("Selecione uma das opções acima: "))
-        if opcao_secundaria != 5:
+        try:
+          opcao_secundaria = int(input("Selecione uma das opções acima: "))
+        except ValueError:
+          opcao_secundaria = -1
+        if  opcao_secundaria == 1 or opcao_secundaria == 2:
             print(f"Você selecionou a opção {opcao_secundaria}.")
+        elif opcao_secundaria == 3 or opcao_secundaria == 4:
+            print("EM DESENVOLVIMENTO")
+        elif opcao_secundaria == 5:
+           break
         else :
-          break
+          print("Opção inválida!")
+  elif 2 <= opcao < 6:
+     print("EM DESENVOLVIMENTO")
+  else:
+     print("Opção inválida!")
 print("Obrigado e volte sempre!")
