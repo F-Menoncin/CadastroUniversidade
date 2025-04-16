45a045fd14a4f20ad97a11c94b622381ae6878e6
Felipe <felipemenoncin1@gmail.com>
2025-04-16 15:22:47 -0300

    Opção alterar estudante

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ lista_estudantes = []
 #while True para que o loop seja infinito (tem que por um break no meio senão fica infinito) 
 while True:
     #apresentando o menu inicial
-  print("\nBem vindo ao menu principal!\n")
-  print("1. Estudantes")
-  print("2. Professores")
-  print("3. Disciplinas")
-  print("4. Turmas")
-  print("5. Matrículas")
-  print("6. Sair\n")
+  print("\nBem vindo ao menu principal!\n1. Estudantes\n2. Professores\n3. Disciplinas\n4. Turmas\n5. Matrículas\n6. Sair\n")
 
   #recebendo a resposta do usuário (int para receber a variavel no formato numero ao invés de texto)
   try:
@@ -24,18 +18,13 @@ while True:
       break
   elif  opcao == 1: 
       while True:
-        print("\n===== MENU DE OPERAÇÕES =====\n")
-        print("Menu de Estudantes\n")
-        print("1. Incluir")
-        print("2. Listar")
-        print("3. Excluir")
-        print("4. Alterar")
-        print("5. Retornar ao menu principal\n")
+        print("\n===== MENU DE OPERAÇÕES =====\nMenu de Estudantes\n1. Incluir\n2. Listar\n3. Excluir\n4. Alterar\n5. Retornar ao menu principal\n")
         #try para não receber um valor inválido e dar erro (usar o except)
         try:
           opcao_secundaria = int(input("Selecione uma das opções acima: "))
         except ValueError:
           opcao_secundaria = -1
+
         if  opcao_secundaria == 1:
           while True:
               print("\nVocê selecionou a opção de incluir um estudante!")
@@ -49,51 +38,104 @@ while True:
                    print("O código digitado é inválido!")
                    break
                 cpf = input('\nDigite o CPF do estudante: ')
+                #dicionário armazenando três variáveis sobre um estudante
                 novo_estudante = {
                   "cod_estudante" : codigo,
                   "nome_estudante" : estudante,
                   "cpf_estudante" : cpf 
                 }
                 #append para que a lista possa ser flexível e tenha um número de entradas variável
-                #cadastro_estudantes.append(estudante) 
+                #adicionando o dicionário como uma entrada de uma lista 
                 lista_estudantes.append(novo_estudante)
                 print("\nEstudante adicionado com sucesso!")
+
         elif  opcao_secundaria == 2:
             #len(variavel) é uma forma de verificar se a lista esta vazia, pois se o comprimento dela (length) for zero, ela está vazia.
             print("\nVocê selecionou a opção de listar os estudantes cadastrados!")
             if len(lista_estudantes) == 0:
-              print("\nNão há estudantes cadastrados!\n")
+              print("\nNão há estudantes cadastrados!")
             else:
               print("\nAqui está a lista de estudantes: \n")
               #for para listar em linhas diferentes
               for estudante in lista_estudantes:
+                #percorre a lista e printa o nome, codigo e cpf de cada estudante
                 cod = estudante["cod_estudante"]
                 nome = estudante["nome_estudante"]
                 cpf = estudante["cpf_estudante"]
                 print(f"Estudante: {nome}, CPF: {cpf}, código: {cod}.")
-        elif opcao_secundaria == 3:
-            print("\nVocê selecionou a opção de excluir o cadastro um estudante!\n")
-            try:
-              #print("\n===== EM DESENVOLVIMENTO =====")
-              excluir_estudante = int(input("\nInsira o código do estudante que deseja excluir: "))
-              encontrado = False
 
-              for estudante in lista_estudantes:
-                if estudante["cod_estudante"] == excluir_estudante:
-                    lista_estudantes.remove(estudante)
-                    print("Estudante removido com sucesso!")
-                    encontrado =  True
-                    break
-              if encontrado == False:
+        elif opcao_secundaria == 3:
+            if len(lista_estudantes) == 0:
+                print("\nNão há estudantes cadastrados!")
+                #print("\n===== EM DESENVOLVIMENTO =====")
+            else:
+              print("\nVocê selecionou a opção de excluir o cadastro de um estudante!")
+              try:
+                #print("\n===== EM DESENVOLVIMENTO =====")
+                excluir_estudante = int(input("\nInsira o código do estudante que deseja excluir: "))
+                encontrado = False
+
+                for estudante in lista_estudantes:
+                  if estudante["cod_estudante"] == excluir_estudante:
+                      lista_estudantes.remove(estudante)
+                      print("Estudante removido com sucesso!")
+                      encontrado =  True
+                      break
+                if encontrado == False:
                     print("Estudante não encontrado!")
-            except ValueError:
+              except ValueError:
                print("Erro: O código digitado é inválido.")
+
         elif opcao_secundaria == 4:
-            print("\n===== EM DESENVOLVIMENTO =====")
+            if len(lista_estudantes) == 0:
+              print("\nNão há estudantes cadastrados!")
+             #print("\n===== EM DESENVOLVIMENTO =====")
+            else:
+              print("\nVocê selecionou a opção de alterar o cadastro de um estudante!")
+              try:
+                alt_estudante = int(input("\nInsira o código do estudante que deseja alterar: "))
+                encontrado = False
+
+                for estudante in lista_estudantes:
+                  if estudante["cod_estudante"] == alt_estudante:
+                      print("\n1. Nome\n2. CPF\n3. Código")
+                      try:
+                        o_que_alterar = int(input("\nSelecione o que deseja alterar: "))
+                      except ValueError:
+                         print("Opção inválida")
+                      if o_que_alterar == 1:
+                          estudante["nome_estudante"] = input("Digite o novo nome: ")
+                          encontrado =  True
+                          print("Nome alterado com sucesso!")
+                          break
+                      elif o_que_alterar == 2:
+                          estudante["cpf_estudante"] = input("Digite o novo CPF: ")
+                          encontrado =  True
+                          print("CPF alterado com sucesso!")
+                          break
+                      elif o_que_alterar == 3:
+                          try:
+                            estudante["cod_estudante"] = int(input("Digite o novo código: "))
+                          except ValueError:
+                             print("Valor inválido.")
+                             break
+                          encontrado =  True
+                          print("Código alterado com sucesso!")
+                          break
+                      else:
+                         print("Opção inválida.")
+                      break
+                if encontrado == False:
+                      print("Estudante não encontrado!")
+              except ValueError:
+                print("Erro: O código digitado é inválido.") 
+
+
         elif opcao_secundaria == 5:
            break
         else :
           print("\nOpção inválida!\n")
+
   elif 2 <= opcao < 6:
      print("\n===== EM DESENVOLVIMENTO =====\n")
   else:
